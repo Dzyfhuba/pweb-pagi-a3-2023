fc846de961e56e4561f0472a9a1e9c899df74241
Dzyfhuba <hafidz21ub@gmail.com>
2023-05-30 14:01:15 +0700

    add menu trigger

---
+++ navbar.js
@@ -0,0 +1,20 @@
+const bukaMenu = () => {
+  console.log('buka menu: ' + open)
+  open = !open
+  if (open) {
+    menu.classList.add('open')
+  } else {
+    menu.classList.remove('open')
+  }
+}
+
+const menuTrigger = document.querySelector('.menu-trigger')
+const menu = document.querySelector('.navbar-menu')
+
+// console.log(menuTrigger)
+// console.log(menu)
+
+let open = false
+menuTrigger.addEventListener('click', bukaMenu)
+// menuTrigger.addEventListener('mouseenter', () => console.log('masuk'))
+// menuTrigger.addEventListener('mouseleave', () => console.log('keluar'))
